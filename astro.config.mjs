713aaa8d14df1cea43850db625b379a8e3218505
rohan <rss2086@gmail.com>
2023-09-29 15:04:45 -0700

    added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,30 +3,39 @@ import tailwind from "@astrojs/tailwind";
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import react from "@astrojs/react";
-import basicSsl from '@vitejs/plugin-basic-ssl'
-
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import vercel from "@astrojs/vercel/serverless";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ storyblok({
+  integrations: [storyblok({
     accessToken: env.STORYBLOK_TOKEN,
     components: {
       team: 'storyblok/TeamPage',
       blogPost: 'storyblok/BlogPost',
       blogPostList: 'storyblok/BlogPostList',
-      page: 'storyblok/Page',
+      page: 'storyblok/Page'
     },
     apiOptions: {
       // Choose your Storyblok space region
-      region: 'us', // optional,  or 'eu' (default)
-    },
+      region: 'us' // optional,  or 'eu' (default)
+    }
   }), tailwind(), react()],
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true,
-    },
+      https: true
+    }
   },
+  output: "hybrid",
+  adapter: vercel({
+    webAnalytics: {
+      enabled:true
+    },
+    speedInsights: {
+      enabled: true
+    }
+  })
 });
