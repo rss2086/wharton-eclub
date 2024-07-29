f6b7fba556c9bc2d5cb40b557950b06848aae21f
Rohan Shah <rss2086@gmail.com>
2024-07-29 08:42:43 -0700

    website update complete to have up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -90,9 +90,6 @@ const navigation = {
               </a>
             ))}
           </div>
-          <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-            &copy; 2020 Your Company, Inc. All rights reserved.
-          </p>
         </div>
       </footer>
     )
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,8 @@
 
-import React from "react";
 import { motion } from 'framer-motion';
 import { useLottie } from "lottie-react";
 import rocket from "../small-rocket.json";
+import EClubLogo from '../images/eclub-logo.png'
 
 export default function HeroSection() {
   const { View: RocketAnimation } = useLottie({
@@ -11,7 +11,7 @@ export default function HeroSection() {
   });
 
   return (
-    <div className="relative overflow-hidden bg-gradient-to-br from-red-900 via-red-950 to-blue-950 text-white min-h-screen">
+    <div className="relative overflow-hidden bg-gradient-to-br from-zinc-900 via-zinc-950 to-blue-950 text-white min-h-screen px-4">
       <div className="absolute inset-0 bg-[url('/grid.svg')] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))] z-0"></div>
       <div className="absolute inset-0 opacity-20 bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCI+CjxyZWN0IHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgZmlsbD0iIzE3MjU0YyI+PC9yZWN0Pgo8Y2lyY2xlIGN4PSIzMCIgY3k9IjMwIiByPSIyIiBmaWxsPSIjZmZmZmZmIiBvcGFjaXR5PSIwLjQiPjwvY2lyY2xlPgo8Y2lyY2xlIGN4PSIwIiBjeT0iMzAiIHI9IjIiIGZpbGw9IiNmZmZmZmYiIG9wYWNpdHk9IjAuNCI+PC9jaXJjbGU+CjxjaXJjbGUgY3g9IjYwIiBjeT0iMzAiIHI9IjIiIGZpbGw9IiNmZmZmZmYiIG9wYWNpdHk9IjAuNCI+PC9jaXJjbGU+CjxjaXJjbGUgY3g9IjMwIiBjeT0iMCIgcj0iMiIgZmlsbD0iI2ZmZmZmZiIgb3BhY2l0eT0iMC40Ij48L2NpcmNsZT4KPGNpcmNsZSBjeD0iMzAiIGN5PSI2MCIgcj0iMiIgZmlsbD0iI2ZmZmZmZiIgb3BhY2l0eT0iMC40Ij48L2NpcmNsZT4KPC9zdmc+')] bg-repeat z-0"></div>
       <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:py-32 py-4 z-20">
@@ -22,22 +22,26 @@ export default function HeroSection() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
           >
-            <h1 className="mt-10 max-w-lg text-5xl font-bold tracking-tight text-white sm:text-7xl">
-              Meet Entrepreneurship at <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-blue-400">Wharton</span>
+            <div className="flex justify-start items-center space-x-4">
+              {/* <img src={WhartonLogo.src} alt="Wharton Logo" className="h-12 w-auto" />  */}
+              <img src={EClubLogo.src} alt="EClub Logo" className="h-16 w-auto invert" />
+              </div>
+            <h1 className=" max-w-lg text-4xl font-bold tracking-tight text-white sm:text-6xl pt-4">
+              The home for entrepreneurship at <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-blue-400">Wharton</span>
             </h1>
             <p className="mt-6 text-xl leading-8 text-gray-300 md:text-3xl">
               E-Club is Wharton's largest student-run organization focused on entrepreneurship. We provide resources, events, and networking opportunities for aspiring founders.
             </p>
-            <div className="mt-10 flex flex-col sm:flex-row items-center gap-y-4 sm:gap-x-6">
+            <div className="mt-10 flex flex-col sm:flex-row items-start sm:items-center gap-y-4 sm:gap-x-6">
               <a
                 href="https://groups.wharton.upenn.edu/student_community?club_id=11514"
-                className=" sm:w-auto rounded-full bg-white px-6 py-3 text-base sm:text-lg font-semibold text-indigo-600 shadow-sm hover:bg-indigo-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white transition-all duration-300 text-center"
+                className="sm:w-auto rounded-md bg-white px-6 py-3 text-base sm:text-lg font-semibold text-red-600 shadow-sm hover:bg-indigo-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white transition-all duration-300 text-left"
               >
-                Explore student startups
+                Join now
               </a>
-              <a href="#learn-more" className="text-base sm:text-lg font-semibold leading-6 text-white hover:text-indigo-200 transition-colors duration-300 text-center sm:text-left">
+              {/* <a href="#learn-more" className="text-base sm:text-lg font-semibold leading-6 text-white hover:text-indigo-200 transition-colors duration-300 text-left">
                 See resources <span aria-hidden="true">→</span>
-              </a>
+              </a> */}
             </div>
           </motion.div>
         </div>
@@ -51,7 +55,7 @@ export default function HeroSection() {
             opacity: [0, 1, 1]
           }}
           transition={{ 
-            duration: 2, 
+            duration: 1.5, 
             times: [0, 0.6, 1],
             ease: ["easeOut", "easeIn"]
           }}
@@ -63,7 +67,6 @@ export default function HeroSection() {
           </div>
         </motion.div>
       </div>
-
     </div>
   );
 }
